b24e78814c63ad26248948a0da79e05124d7220f
Vitor26laser <97393601+Vitor23laser@users.noreply.github.com>
2025-06-23 10:51:46 -0300

    Update start_ai_server.bat

--- toontown/dev/win32/start_ai_server.bat
+++ toontown/dev/win32/start_ai_server.bat
@@ -3,6 +3,9 @@ title Toontown Online #_BR #JP #FR #ES - AI Server
 cd ../..
 
 :main 
-$WINTOOLS/bin/python.exe -m toontown.ai.AIStart #-m toontown.ai.ServiceStart #-m toontown.ai.UlityStart $TOONTOWN/src/launcher/Configrc.prc QA e EN 2003-2010=$OTP/built/Configrc.prc EN 2011=$TOONTOWN/src/launcher/client_Configrc.prc
-pause
-goto :main
+$WINTOOLS/bin/python.exe -m toontown.ai.AIStart #-m toontown.ai.ServiceStart #-m toontown.ai.UlityStart --base-channel 401000000 ^
+               --max-channels 999999 --stateserver 4002 ^
+               --messagedirector-ip localhost ^
+               --eventlogger-ip localhost ^ #colocar seu servidor aqui
+               --district-name "Toon Valley" #--disctrict-name "Altos dos Irados" #BR
+goto main
