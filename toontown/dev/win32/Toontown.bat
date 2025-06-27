2cecffc9b5458627c2b3eebf62b650a521dfe9b2
Vitor26laser <97393601+Vitor23laser@users.noreply.github.com>
2025-06-27 08:51:06 -0300

    Update Toontown.bat

--- toontown/dev/win32/Toontown.bat
+++ toontown/dev/win32/Toontown.bat
@@ -2,6 +2,6 @@
 title Toontown Online #BR #FR #JP #ES - Game Client
 cd..
 
-$WINTOOLS/bin/python.exe -m toontown.launcher.StartToontownLauncher #2005 a 2011
+$WINTOOLS/bin/python.exe -m toontown.launcher.StartToontownLauncher #2005 a #2011
 $WINTOOLS/bin/python.exe  -m toontown.launcher.QuickStartLauncher #2013
 pause
